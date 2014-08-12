e26fdd8c79af8fd4b34709dd7e1b9464064f7be1
bill37chen <bill37chen@yeah.net>
2014-08-12 12:15:33 +0800

    test pycharm's vcs

--- myplatform/myplatform/settings.py
+++ myplatform/myplatform/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'baseplatform',
+    'base',
     'django.contrib.admin',
 )
 
